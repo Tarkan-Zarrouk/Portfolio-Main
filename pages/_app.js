77116376a47c6ad1cd73e3bbdac0a5f603fb2523
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-08-18 05:19:35 -0400

    revert

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import "@/styles/globals.css";
+// pages/_app.js or pages/_app.tsx
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
