bf5753ffc0e955dca29e12ab26d64d91dc679f86
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-08-17 23:25:07 -0400

    Added Icons for Libraries & Frameworks && other tools

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,13 @@ import PythonIcon from "@/public/python_icon.png";
 import JavascriptIcon from "@/public/javascript_icon.png"
 import GitIcon from "@/public/git_icon.png";
 import GitHubIcon from "@/public/github_icon.png";
+import ReactIcon from "@/public/react_icon.png";
+import TailwindIcon from "@/public/tailwindcss_icon.png";
+import BootstrapIcon from "@/public/bootstrap_icon.png";
+import SassIcon from "@/public/sass_icon.png";
+import WindowsIcon from "@/public/windows_icon.png";
+import PopOsIcon from "@/public/popos_icon.png";
+import MacIosOS from "@/public/macosios_icon.png";
 
 
 export default function Home() {
@@ -38,7 +45,7 @@ export default function Home() {
             </a>
             <Link href="https://www.linkedin.com/in/tarkan-zarrouk" target="_blank">
               <Button gap="5px" _hover={{ bg: "blue.300", color: "black" }}>
-                Contact Me
+                Connect With Me
                 <IoMail />
               </Button>
             </Link>
@@ -66,12 +73,12 @@ export default function Home() {
                   <Image src={CssIcon} width="40" />
                 </Button>
               </Tooltip>
-              <Tooltip hasArrow label="Python From 2024 - Present" borderRadius="1rem">
+              <Tooltip hasArrow label="Python From 2024 - Present" placement="bottom" borderRadius="1rem">
                 <Button mt="1rem" borderRadius="50%" height="4rem" width="auto" bg="white" ml="10px">
                   <Image src={PythonIcon} width="40" />
                 </Button>
               </Tooltip>
-              <Tooltip hasArrow label="JavaScript From 2023 - Present" borderRadius="1rem">
+              <Tooltip hasArrow label="JavaScript From 2023 - Present" placement="bottom" borderRadius="1rem">
                 <Button mt="1rem" borderRadius="50%" height="4rem" width="auto" bg="white" ml="10px">
                   <Image src={JavascriptIcon} width="40" />
                 </Button>
@@ -80,18 +87,63 @@ export default function Home() {
               <br />
               <Text as="b" textDecor="underline" fontSize="2xl">Version Control Management</Text>
               <br />
-              <Tooltip hasArrow label="Git From 2022 - Present">
+              <Tooltip hasArrow label="Git From 2022 - Present" placement="bottom">
                 <Button mt="1rem" borderRadius="50%" height="4rem" width="auto" bg="white" ml="10px">
                   <Image src={GitIcon} width="40" />
                 </Button>
               </Tooltip>
-              <Tooltip hasArrow label="GitHub 2021 - Present">
+              <Tooltip hasArrow label="GitHub 2021 - Present" placement="bottom" borderRadius="1rem">
                 <Button mt="1rem" borderRadius="50%" height="4rem" width="auto" bg="white" ml="10px">
                   <Image src={GitHubIcon} width="40" />
                 </Button>
               </Tooltip>
             </Box>
-            <Box bg='tomato' height='80px'>
+            <Box pl="1rem">
+              <Text as="b" fontSize="2xl" textDecor="underline">
+                Libraries & Frameworks
+              </Text>
+              <br />
+              <Tooltip hasArrow label="React.JS From 2024 - Present" placement="bottom" borderRadius="1rem">
+                <Button mt="1rem" borderRadius="50%" height="4rem" width="auto" bg="white" ml="10px">
+                  <Image src={ReactIcon} width="40" />
+                </Button>
+              </Tooltip>
+              <Tooltip hasArrow label="TailwindCSS From 2024 - Present" placement="bottom" borderRadius="1rem">
+                <Button mt="1rem" borderRadius="50%" height="4rem" width="auto" bg="white" ml="10px">
+                  <Image src={TailwindIcon} width="40" />
+                </Button>
+              </Tooltip>
+              <Tooltip hasArrow label="Bootstrap From 2023 - Present" placement="bottom" borderRadius="1rem">
+                <Button mt="1rem" borderRadius="50%" height="4rem" width="auto" bg="white" ml="10px">
+                  <Image src={BootstrapIcon} width="40" />                  
+                </Button>
+              </Tooltip>
+              <Tooltip hasArrow label="SasS From 2022 - Present" placement="bottom" borderRadius="1rem">
+                <Button mt="1rem" borderRadius="50%" height="4rem" width="auto" bg="white" ml="10px">
+                  <Image src={SassIcon} width="40" />
+                </Button>
+              </Tooltip>
+              <br />
+              <br />
+              <Text as="b" fontSize="2xl" textDecor="underline">
+                Other Tools & / Or Services
+              </Text>
+              <br />
+              <Tooltip hasArrow label="Windows From 2014 - Present" placement="bottom" borderRadius="1rem">
+                <Button mt="1rem" borderRadius="50%" height="4rem" width="auto" bg="white" ml="10px">
+                  <Image src={WindowsIcon} width="40" />
+                </Button>
+              </Tooltip>
+              <Tooltip hasArrow label="PopOs! From 2019 - 2020" placement="bottom" borderRadius="1rem">
+                <Button mt="1rem" borderRadius="50%" height="4rem" width="auto" bg="white" ml="10px">
+                  <Image src={PopOsIcon} width="40" />
+                </Button>
+              </Tooltip>
+              <Tooltip hasArrow label="iOS & MacOS From 2018 - Present & 2024 - Present" placement="bottom" borderRadius="1rem">
+                <Button mt="1rem" borderRadius="50%" height="4rem" width="auto" bg="white" ml="10px">
+                  <Image src={MacIosOS} width="40" />
+                </Button>
+              </Tooltip>
             </Box>
           </SimpleGrid>
         </Box>
