ab4983cbd5e6e46ae892c8ecfb5f13ed91337c72
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-08-18 05:04:08 -0400

    test fix #3

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
       >
         <Box textAlign="center">
           <Text fontSize="4xl" fontWeight="bold">
-            Hi, I&apos;m Tarkan 👋
+            {`Hi, I'm Tarkan 👋`}
           </Text>
           <Text fontSize="md" w={["100%", "50%"]} mx={["none", "auto"]} color="gray.500">
             Hi, I’m Tarkan Zarrouk, a 16-year-old self-taught web developer passionate about creating websites and eager to turn this into a full-fledged career. I love solving problems, learning new technologies, and can’t wait to collaborate with other developers to push the boundaries of what’s possible on the web.
